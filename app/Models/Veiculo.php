9e3dde221aba103a6ac232aa92da704355f6d76b
devmarcosav@gmail.com <devmarcosav@gmail.com>
2024-01-11 22:28:53 -0300

    feat: create Model Veiculo

---
+++ app/Models/Veiculo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Veiculo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['pessoa_id', 'marca', 'tipo', 'modelo', 'placa'];
+    
+    public function pessoa()
+    {
+        return $this->belongsTo(Pessoa::class);
+    }
+
+    public function revisoes()
+    {
+        return $this->hasMany(Revisao::class);
+    }
+}
